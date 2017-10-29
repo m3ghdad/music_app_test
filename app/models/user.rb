2294fda59b740197d6095258d880cf2868750318
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-10-29 11:04:14 -0700

    sets up table and model for users

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates :password_digest, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :session_token, presence: true, uniqueness: true
+
+  validates :password, length: {minimum: 6, allow_nil: true}
+end
