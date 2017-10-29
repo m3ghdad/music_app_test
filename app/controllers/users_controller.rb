d6846ab40dec68753b132e20191df716b5c2a9a1
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-10-29 11:47:46 -0700

    Adds current_user and login logic inside application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       redirect_to users_url
+      #login!(user)
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
