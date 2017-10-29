cd569da8c4cfcddd6aadd1aa175d486cc8da8d6c
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-10-29 11:28:18 -0700

    Adds logic for setting up password for user and look_by_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,16 @@ class User < ApplicationRecord
   validates :password, length: {minimum: 6, allow_nil: true}
 
   after_initialize :ensure_session_token
-  
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+
+  end
+
   def self.generate_session_token
     SecureRandom.urlsafe_base64(16)
   end
@@ -32,6 +41,15 @@ class User < ApplicationRecord
 
   def ensure_session_token
     self.session_token ||= User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new.(self.password_digest).is_password?(password)
   end
 
 end
