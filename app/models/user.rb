c4f5baba384d6119d81ad6cf7748b44d509fa018
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-10-29 11:10:33 -0700

    Adds session token for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,22 @@ class User < ApplicationRecord
   validates :session_token, presence: true, uniqueness: true
 
   validates :password, length: {minimum: 6, allow_nil: true}
+
+  after_initialize :ensure_session_token
+  
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
 end
