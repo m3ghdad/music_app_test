bf4f620ada612b15e7ae28030a391dab38d22594
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-10-29 17:53:04 -0700

    Adds validation tests for User and sets up shoulda_matchers in rails_helper.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,27 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id              :integer          not null, primary key
-#  email           :string           not null
-#  password_digest :string           not null
-#  session_token   :string           not null
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#
-
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject(:user) do
+    User.new(email: 'm@m.com', password:'123456')
+  end
+
+  describe 'validations' do
+
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    # it {should validate_uniqueness_of(:email) }
+    # it {should validate_presence_of(:session_token)}
+    # it {should validate_uniqueness_of(:session_token)}
+  end
+
+
+  describe 'associations' do
+
+  end
+
+  describe 'class methods' do
+
+  end
+
 end
